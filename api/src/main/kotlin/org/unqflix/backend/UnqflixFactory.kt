07375e1c2de650d18f034313f89c5ab26ef8208f
Federico Agustin Sandoval <federico.sandoval@alu.unq.edu.ar>
2020-07-01 20:33:16 -0300

    ♻ cambio links de videos por otros reales

--- api/src/main/kotlin/org/unqflix/backend/UnqflixFactory.kt
+++ api/src/main/kotlin/org/unqflix/backend/UnqflixFactory.kt
@@ -41,16 +41,16 @@ object UnqflixFactory {
         )
 
         var yn= Movie(idGenerator.nextMovieId(),"your name","Una linda historia.",
-            "https://images-na.ssl-images-amazon.com/images/I/71-WBN3FCBL._AC_SY741_.jpg",Available(),"www.yourname.com/movie.mp4",112,
+            "https://images-na.ssl-images-amazon.com/images/I/71-WBN3FCBL._AC_SY741_.jpg",Available(),"https://storage.googleapis.com/omega-pivot-269518.appspot.com/cv5311514lt.mp4",112,
             categories = mutableListOf(baseCats[1],baseCats[6]))
 
         var jw= Movie(idGenerator.nextMovieId(),"john wick","El gran John",
-            "https://images-na.ssl-images-amazon.com/images/I/71WiYBT2QsL._AC_SY741_.jpg",Available(),"johnwick.com/movie.mp4",107,
+            "https://images-na.ssl-images-amazon.com/images/I/71WiYBT2QsL._AC_SY741_.jpg",Available(),"https://repelisgoo.net/srv-pv/crypto/b/v9Aj-7qcRcW2VA4y5ovdt8aRuCG4h2dl0Be7nH_cOLFlu1IVcr6agIIWyoLLKoQagRipSSznmVoo8usOPvhKRftaICChBVV9TWSBzwy8MM7EP6MpguWx7O8YC0NkCiE9F20tG4V3aNw3ojpVZgmWDIrGDyuHO-Ip36Ped6vDGmg_1yHTmsvkEHTrCIvI-xJjkO9Ji4ar5mubtxaiNVtFCw/382320C8467040D3192DA76EFA6189C3670F9FDC1699DBFA32E9865779ADBB4E?qs=1&youtubeTrailer=UNIv8nzUryo",107,
             categories = mutableListOf(baseCats[3]))
 
         var delr= Movie(idGenerator.nextMovieId(),"y donde estan las rubias?","Los detectives que se visten " +
                 "de mujeres para cumplir su mision",
-            "https://i.pinimg.com/originals/c4/ad/6b/c4ad6b40be573f8cf2c7459447318f7d.jpg",Available(),"www.johnwickl.com/movie.mp4",115,
+            "https://i.pinimg.com/originals/c4/ad/6b/c4ad6b40be573f8cf2c7459447318f7d.jpg",Available(),"https://fvs.io/redirector?token=cU56RDBQWGFuREJVSVZhbkVaU2s2bU1xbDVjUCtvaGJTQnZZQll6K2V0djBVMlJXNUF1REVkU2kwcUUzMTNEbkM2VmxUSDYxK01PbHZkTlFKdzg1WWhUcStTSHhJcUJwYWxHaVpqQWhsZFNuNzZsUW1pQWRrdXdRTFV0RFEvMC84dGgyalhFenFmSmo4Wk8wOHB0T2VubmZFZEpCb1E9PTptUkhuVHhYN1ZkVW1LNkJRc3BOZlpnPT0",115,
             mutableListOf("Shawn Wayans", "Marlon Wayans","John Heard"), mutableListOf("Keenen Ivory Wayans"),
             mutableListOf(baseCats[0]), mutableListOf(yn,jw))
 
@@ -68,7 +68,7 @@ object UnqflixFactory {
         mutableListOf(oneP,snk))
 
         unqflixModel = UNQFlix(series = mutableListOf(oneP, gint, dn, snk, opm, dbz), movies = mutableListOf(yn,jw,delr),
-            categories = baseCats, users = mutableListOf(nico,uli,chester), banners = mutableListOf(jw,gint,oneP,yn))
+            categories = baseCats, users = mutableListOf(nico,uli,chester), banners = mutableListOf(jw,gint,oneP,yn,opm))
     }
 
 
@@ -99,10 +99,10 @@ object UnqflixFactory {
 
         var chapter1 = Chapter(
             idGenerator.nextChapterId(),"Volamos hacia el espacio!","Termina la batalla entre saiyans" +
-                    " y deciden ir a Namekusei.",60,"dragonballz.com/sagadefreezer1.mp4","")
+                    " y deciden ir a Namekusei.",60,"https://videos2.sendvid.com/8c/86/nceb2qe3.mp4?validfrom=1593642256&validto=1593649456&rate=200k&burst=1000k&ip=201.176.192.99&hash=xSwERa%2FjnHCafzdxdXqPciN4PDo%3D","")
         var chapter2 = Chapter(
             idGenerator.nextChapterId(),"El misterio de yunzabit!","La busqueda de la nave espacial de Dios.",
-            90,"dragonballz.com/sagadefreezer2.mp4","")
+            90,"https://videos2.sendvid.com/3d/79/a1lscyw0.mp4?validfrom=1593642368&validto=1593649568&rate=200k&burst=1000k&ip=201.176.192.99&hash=2mdGCMdKqHgwMFTnWD6Q5qYq82I%3D","")
         return mutableListOf(chapter1,chapter2)
     }
 
