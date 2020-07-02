5a6160bdaed139dade928391298cb4f6f3268d14
uliseslopez98 <uliseslopez1112@gmail.com>
2020-07-02 18:17:32 -0300

    refactor en la api y logo de la app
    links a videos de youtube (en api) y efecto en el boton del home

--- api/src/main/kotlin/org/unqflix/backend/UnqflixFactory.kt
+++ api/src/main/kotlin/org/unqflix/backend/UnqflixFactory.kt
@@ -48,12 +48,12 @@ object UnqflixFactory {
             categories = mutableListOf(baseCats[1],baseCats[6]))
 
         var jw= Movie(idGenerator.nextMovieId(),"john wick","El gran John",
-            "https://images-na.ssl-images-amazon.com/images/I/71WiYBT2QsL._AC_SY741_.jpg",Available(),"https://repelisgoo.net/srv-pv/crypto/b/v9Aj-7qcRcW2VA4y5ovdt8aRuCG4h2dl0Be7nH_cOLFlu1IVcr6agIIWyoLLKoQagRipSSznmVoo8usOPvhKRftaICChBVV9TWSBzwy8MM7EP6MpguWx7O8YC0NkCiE9F20tG4V3aNw3ojpVZgmWDIrGDyuHO-Ip36Ped6vDGmg_1yHTmsvkEHTrCIvI-xJjkO9Ji4ar5mubtxaiNVtFCw/382320C8467040D3192DA76EFA6189C3670F9FDC1699DBFA32E9865779ADBB4E?qs=1&youtubeTrailer=UNIv8nzUryo",107,
+            "https://images-na.ssl-images-amazon.com/images/I/71WiYBT2QsL._AC_SY741_.jpg",Available(),"https://www.youtube.com/embed/NSI54Cvu7IU",107,
             categories = mutableListOf(baseCats[3]))
 
         var delr= Movie(idGenerator.nextMovieId(),"y donde estan las rubias?","Los detectives que se visten " +
                 "de mujeres para cumplir su mision",
-            "https://i.pinimg.com/originals/c4/ad/6b/c4ad6b40be573f8cf2c7459447318f7d.jpg",Available(),"https://fvs.io/redirector?token=cU56RDBQWGFuREJVSVZhbkVaU2s2bU1xbDVjUCtvaGJTQnZZQll6K2V0djBVMlJXNUF1REVkU2kwcUUzMTNEbkM2VmxUSDYxK01PbHZkTlFKdzg1WWhUcStTSHhJcUJwYWxHaVpqQWhsZFNuNzZsUW1pQWRrdXdRTFV0RFEvMC84dGgyalhFenFmSmo4Wk8wOHB0T2VubmZFZEpCb1E9PTptUkhuVHhYN1ZkVW1LNkJRc3BOZlpnPT0",115,
+            "https://i.pinimg.com/originals/c4/ad/6b/c4ad6b40be573f8cf2c7459447318f7d.jpg",Available(),"https://www.youtube.com/embed/Cz16XzYe1A4",115,
             mutableListOf("Shawn Wayans", "Marlon Wayans","John Heard"), mutableListOf("Keenen Ivory Wayans"),
             mutableListOf(baseCats[0]), mutableListOf(yn,jw))
 
@@ -77,7 +77,7 @@ object UnqflixFactory {
     private fun onePSeason(): MutableList<Season> {
         var chapter1 = Chapter(
             idGenerator.nextChapterId(),"¡Soy Luffy! ¡El hombre que se convertirá en el Rey de los Piratas!","Start chapter",60,
-            "https://www888.o0-2.com/token=SGop8vq0AnwRfKSPrCuJ0w/1593735487/201.176.0.0/38/3/2d/9e4e9f0f0e7f622af422d2dca41432d3-480p.mp4","")
+            "https://www.youtube.com/embed/9DYfJfkwPnE","")
         var baseCh = mutableListOf(chapter1)
         var season = Season(
             idGenerator.nextSeasonId(),"Season 1","First Season",
@@ -88,7 +88,7 @@ object UnqflixFactory {
     private fun dnSeason(): MutableList<Season> {
         var chapter1 = Chapter(
             idGenerator.nextChapterId(),"Renacimiento.","Start chapter",60,
-            "https://www66.o0-1.com/token=KCA4fGp3B2MWzkdtHVRWHA/1593735319/201.176.0.0/28/1/ea/01cc337a1707d27b4eedce8586e66ea1-480p.mp4","")
+            "https://www.youtube.com/embed/7lrZpmrL2BE","")
         var baseCh = mutableListOf(chapter1)
         var season = Season(
             idGenerator.nextSeasonId(),"Season 1","First Season",
@@ -99,7 +99,7 @@ object UnqflixFactory {
     private fun opmSeason(): MutableList<Season> {
         var chapter1 = Chapter(
             idGenerator.nextChapterId(),"El hombre más poderoso del mundo","Start chapter",60,
-            "https://www1140.o0-3.com/token=1IRwH0v2Zvz1dzvJnB8OTA/1593735073/201.176.0.0/38/9/e9/b0427ab32a9a22af565ce843eb953e99-480p.mp4","")
+            "https://www.youtube.com/embed/fth2hSLJSmg","")
         var baseCh = mutableListOf(chapter1)
         var season = Season(
             idGenerator.nextSeasonId(),"Season 1","First Season",
@@ -111,7 +111,7 @@ object UnqflixFactory {
         var chapter1 = Chapter(
             idGenerator.nextChapterId(),"A ti, dentro de 2000 años - La caída de Shiganshina, parte 1!",
             "Start chapter",60,
-            "https://www337.o0-1.com/token=KrdQqJE2s9GnYG2iLUr92w/1593734778/201.176.0.0/31/f/46/801a23a358be1d681659dc2f3ee3946f-480p.mp4","")
+            "https://www.youtube.com/embed/dOjF25b0TKw","")
         var baseCh = mutableListOf(chapter1)
         var season = Season(
             idGenerator.nextSeasonId(),"Season 1","First Season",
@@ -135,10 +135,10 @@ object UnqflixFactory {
 
         var chapter1 = Chapter(
             idGenerator.nextChapterId(),"Volamos hacia el espacio!","Termina la batalla entre saiyans" +
-                    " y deciden ir a Namekusei.",60,"https://www813.o0-2.com/token=UilehDaMu3mjxmALssLJWw/1593733829/201.176.0.0/34/d/23/233436bf2c2841c14228a3adeb7c623d-480p.mp4","")
+                    " y deciden ir a Namekusei.",60,"https://www.youtube.com/embed/k4oHXH5gkeA","")
         var chapter2 = Chapter(
             idGenerator.nextChapterId(),"El misterio de yunzabit!","La busqueda de la nave espacial de Dios.",
-            90,"https://www765.o0-2.com/token=08cEcdT28w7367DYaYNgBA/1593733967/201.176.0.0/34/8/02/273c0347a2e46ded18bd688719c27028-480p.mp4","")
+            90,"https://www.youtube.com/embed/nnJFk_yg3xQ","")
         return mutableListOf(chapter1,chapter2)
     }
 
